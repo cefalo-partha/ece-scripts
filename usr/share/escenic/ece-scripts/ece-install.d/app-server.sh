ef2c62174292790a3328260c3df7640813f411b0
Partha Guha Roy <partha@cefalo.com>
2015-02-05 19:48:58 +0600

    NG-3943: Make it possible to configure heap size for all-in-one instances.

--- usr/share/escenic/ece-scripts/ece-install.d/app-server.sh
+++ usr/share/escenic/ece-scripts/ece-install.d/app-server.sh
@@ -533,6 +533,8 @@ function set_appropriate_jvm_heap_sizes() {
     heap_size=${fai_presentation_heap_size-${default_app_server_heap_size}}
   elif [ $install_profile_number -eq $PROFILE_EDITORIAL_SERVER ]; then
     heap_size=${fai_editor_heap_size-${default_app_server_heap_size}}
+  elif [ $install_profile_number -eq $PROFILE_ALL_IN_ONE ]; then
+    heap_size=${fai_all_heap_size-${default_app_server_heap_size}}
   elif [ $install_profile_number -eq $PROFILE_ANALYSIS_DB_SERVER ]; then
     heap_size=${fai_analysis_heap_size-${default_app_server_heap_size}}
   elif [ $install_profile_number -eq $PROFILE_SEARCH_SERVER ]; then
